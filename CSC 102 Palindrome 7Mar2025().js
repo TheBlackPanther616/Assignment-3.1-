8f6588050a7c023d144ea519b69f745db4aec7d8
TheBlackPanther616 <josephhudsonjr13@gmail.com>
2025-03-07 19:30:32 -0600

    Add files via upload

--- CSC 102 Palindrome 7Mar2025().js
+++ CSC 102 Palindrome 7Mar2025().js
@@ -1,9 +1,12 @@
 function strings()
 {
+    //Establishing variables
    var firstWord = document.getElementById("firstword").value;
    var splitStr = firstWord.split("");
    var reverseStr = splitStr.reverse();
    var joinStr = reverseStr.join("");
+
+   // Establishing arguements for inputs
    if (firstWord == joinStr)
     {
         document.getElementById("Results").innerHTML= "This is a PALINDROME";
